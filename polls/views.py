4c88abe5f66322eb3baab2562581226a7fdf381d
zzzhenng <cjlkindle01@gmail.com>
2018-02-09 12:19:10 +0800

    finished simple vote instance

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
 # from django.http import Http404
 # from django.http import HttpResponse
 # from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -21,8 +24,19 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-  response = "You'ar looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-  return HttpResponse("You'a voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice.",
+      })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
